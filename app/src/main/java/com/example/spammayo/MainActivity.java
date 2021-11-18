3fc8825b52171835a5332df07dfa6fbc1f45eb68
SEUNG <bigguns101>
2021-11-18 12:21:52 +0900

    sd

--- app/src/main/java/com/example/spammayo/MainActivity.java
+++ app/src/main/java/com/example/spammayo/MainActivity.java
@@ -18,20 +18,20 @@ import android.widget.ToggleButton;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static Toast mToast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ToggleButton toggleButton1 = (ToggleButton) findViewById(R.id.toggleButton1);
         toggleButton1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-
                     Toast.makeText(getApplicationContext(), "감시 시스템이 활성화되었습니다", Toast.LENGTH_SHORT).show();
+
                 } else {
                     Toast.makeText(getApplicationContext(), "감시 시스템이 비활성화되었습니다", Toast.LENGTH_SHORT).show();
+
                 }
             }
         });
@@ -42,11 +42,14 @@ public class MainActivity extends AppCompatActivity {
                 if (isChecked) {
 
                     Toast.makeText(getApplicationContext(), "출처를 알 수 없는 앱 설치가 제한되었습니다", Toast.LENGTH_SHORT).show();
+
                 } else {
                     Toast.makeText(getApplicationContext(), "출처를 알 수 없는 앱 설치가 허용되었습니다", Toast.LENGTH_SHORT).show();
+
                 }
             }
         });
+
         ImageButton Button1 = (ImageButton) findViewById(R.id.btn1);
 
         Button1.setOnClickListener(new View.OnClickListener() {
